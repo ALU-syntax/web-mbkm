5afd09527ae913a8a938a335cf696a08fb7cbc4c
ALU-syntax <ardianiqbal40@gmail.com>
2023-05-13 15:37:22 +0700

    create dashboard & forum section

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'index']);
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
